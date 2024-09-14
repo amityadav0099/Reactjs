1131a81d7bf514c83a03067426ec6cf33b5f0ad9
amityadav0099 <yadavamit5675@gmail.com>
2024-09-14 15:04:38 +0530

    Virtual DOM

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -8,3 +8,9 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+
+// virtual DOM
+// createRoot DOM like structure create krta hai jaise browser ke paas DOM hota hai waise e bhi apna DOM create krta hai
+// main DOM ko aur apne DOM ko compare krta hai aur sirf unhi cheez ko update krta hai jo actually me UI me update huie hain
+//  lekin browser pure DOM ko remove krke waps se repaint(structure dubara se taiyaar hota hai) krta hai isi ko page reload bolte hain
+  
